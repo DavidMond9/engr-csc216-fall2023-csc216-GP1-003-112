fd48b363ac26fc9f360aafc45ac51a5761626264
dmmond <dmmond@ncsu.edu>
2023-08-30 15:53:08 -0400

    [Implementation]Implemented methods and handled files using imports.

--- WolfScheduler/src/edu/ncsu/csc216/wolf_scheduler/io/CourseRecordIO.java
+++ WolfScheduler/src/edu/ncsu/csc216/wolf_scheduler/io/CourseRecordIO.java
@@ -1,8 +1,12 @@
 package edu.ncsu.csc216.wolf_scheduler.io;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import edu.ncsu.csc216.wolf_scheduler.course.Course;
 
@@ -21,20 +25,62 @@ public class CourseRecordIO {
      * @return a list of valid Courses
      * @throws FileNotFoundException if the file cannot be found or read
      */
-    public static ArrayList<Course> readCourseRecords(String fileName) throws FileNotFoundException {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	public static ArrayList<Course> readCourseRecords(String fileName) throws FileNotFoundException {
+	    Scanner fileReader = new Scanner(new FileInputStream(fileName));  //Create a file scanner to read the file
+	    ArrayList<Course> courses = new ArrayList<Course>(); //Create an empty array of Course objects
+	    while (fileReader.hasNextLine()) { //While we have more lines in the file
+	        try { //Attempt to do the following
+	            //Read the line, process it in readCourse, and get the object
+	            //If trying to construct a Course in readCourse() results in an exception, flow of control will transfer to the catch block, below
+	            Course course = readCourse(fileReader.nextLine()); 
 
-    /**
+	            //Create a flag to see if the newly created Course is a duplicate of something already in the list  
+	            boolean duplicate = false;
+	            //Look at all the courses in our list
+	            for (int i = 0; i < courses.size(); i++) {
+	                //Get the course at index i
+	                Course current = courses.get(i);
+	                //Check if the name and section are the same
+	                if (course.getName().equals(current.getName()) &&
+	                        course.getSection().equals(current.getSection())) {
+	                    //It's a duplicate!
+	                    duplicate = true;
+	                    break; //We can break out of the loop, no need to continue searching
+	                }
+	            }
+	            //If the course is NOT a duplicate
+	            if (!duplicate) {
+	                courses.add(course); //Add to the ArrayList!
+	            } //Otherwise ignore
+	        } catch (IllegalArgumentException e) {
+	            //The line is invalid b/c we couldn't create a course, skip it!
+	        }
+	    }
+	    //Close the Scanner b/c we're responsible with our file handles
+	    fileReader.close();
+	    //Return the ArrayList with all the courses we read!
+	    return courses;
+	}
+
+    private static Course readCourse(String nextLine) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
      * Writes the given list of Courses to 
      * @param fileName file to write schedule of Courses to
      * @param courses list of Courses to write
      * @throws IOException if cannot write to file
      */
     public static void writeCourseRecords(String fileName, ArrayList<Course> courses) throws IOException {
-        // TODO Auto-generated method stub
-        
+    	PrintStream fileWriter = new PrintStream(new File(fileName));
+
+    	for (int i = 0; i < courses.size(); i++) {
+    	    fileWriter.println(courses.get(i).toString());
+    	}
+
+    	fileWriter.close();
     }
 
 }
